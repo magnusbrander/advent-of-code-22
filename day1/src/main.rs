d4160e1dc83acf6ec0038352d0462a7f0c623f66
Magnus Brander <magnus@bionamic.io>
2023-05-31 18:43:43 +0200

    cleanup

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,32 +1,31 @@
 use std::env;
-use std::fs;
 use util;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
-    let file_content = fs::read_to_string(file_path).expect("Failed to read file");
+    let lines = util::read_file_into_lines(file_path);
 
-    let mut calories_per_elf = get_calories_per_elf(&file_content);
+    let mut calories_per_elf = get_calories_per_elf(&lines);
     util::sort_vec(&mut calories_per_elf, true);
     let max_calories = util::get_max(&calories_per_elf);
     let top_three_sum = util::sum_elements(&calories_per_elf[0..3], 0);
 
-    println!("Max calories: {}", max_calories);
-    println!("Top 3 calories sum: {}", top_three_sum);
+    println!("Max calories: {}, top 3 sum: {}", max_calories, top_three_sum);
 }
 
-fn get_calories_per_elf(file_content: &String) -> Vec<i32> {
-    let mut calories_per_elf: Vec<i32> = Vec::new();
-    let mut current_elf_calories = 0;
-    for line in file_content.lines() {
+fn get_calories_per_elf(lines: &Vec<String>) -> Vec<i32> {
+    let mut cals_per_elf: Vec<i32> = Vec::new();
+    let mut cals_sum = 0;
+    for line in lines {
         if line.is_empty() {
-            calories_per_elf.push(current_elf_calories);
-            current_elf_calories = 0;
+            cals_per_elf.push(cals_sum);
+            cals_sum = 0;
         } else {
-            let calories_on_line: i32 = line.parse().unwrap();
-            current_elf_calories += calories_on_line;
+            let cals = line.parse::<i32>().unwrap(); 
+            cals_sum = cals_sum + cals;
         }
     }
-    return calories_per_elf;
+    cals_per_elf.push(cals_sum);
+    cals_per_elf
 }
